9b323d41bd3f53df95dda6d06aebc7ed0ac68c21
sunnyopenkube <sunny@openkube.com>
2019-02-13 16:49:21 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -5,27 +5,30 @@ import './App.css';
 import Routes from "./Routes";
 import { Link } from "react-router-dom";
 
-// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
-// import requireAuth from "./hocs/AuthenticatedComponent";
-
-// import Home from './components/Marketing/Home/Home';
-// import Login from './components/Authentication/Login/Login';
-// import Register from './components/Authentication/Register/Register';
-// import Dashboard from './components/Product/Dashboard/Dashboard';
-
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isAuthenticated: false
+    };
+  }
+
+  userHasAuthenticated = authenticated => {
+    this.setState({ isAuthenticated: authenticated });
+  }
+
+
   render() {
+
+    const childProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      userHasAuthenticated: this.userHasAuthenticated
+    };
+
     return (
-      // <Router>
-      //   <div>
-      //     <Route exact path="/" component={Home} />
-      //     <Route path="/login" component={Login} />
-      //     <Route path="/register" component={Register} />
-      //     <Route path="/dashboard" component={Dashboard} />
-      //     <Route handler={requireAuth(Dashboard)} name="dashboard"/>
-      //   </div>
-      // </Router>
+
       <div className="App container">
         <div>
           <Link to="/">Home</Link>
@@ -33,7 +36,7 @@ class App extends Component {
           <Link to="/register">Register</Link>
           <Link to="/dashboard">Dashboard</Link>
         </div>
-        <Routes />
+        <Routes childProps={childProps} />
         Footer
       </div>
     );
